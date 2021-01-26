5b82ea24a9466ffd1fc3e247a84fc862bd5df719
mateusrodriguesilva <mateusrodrigues.contato01@gmail.com>
2021-01-26 11:11:53 -0300

    código ao final da segunda aula da imersão react e next.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
 import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import { useState } from 'react';
+
 
 //JavaScript
 /* const BackgroundImage = styled.div`
@@ -26,15 +31,38 @@ export const QuizContainer = styled.div`
 
 
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = React.useState('')//Passamos o estado inicial do nosso componente. Estado inicial do input
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>The legend of zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem opsum dolor sit amet...</p>       
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              
+              router.push(`/quiz?name=${name}`)
+              console.log('Fazendo uma submissão por meio do reat')
+            }}  
+            >
+              <input 
+                onChange={function (infosDoEvento){
+                  //State
+                  //name = infosDoEvento.target.value
+                  setName(infosDoEvento.target.value)//Chama a função e passa o novo estado
+                }}
+              placeholder="" />  
+              <button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form> 
           </Widget.Content>
         </Widget>
 
@@ -46,7 +74,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto"/>
+      <GitHubCorner projectUrl="https://github.com/mateusrodriguesdasilva"/>
     </QuizBackground>
   );
 }
