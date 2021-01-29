69904788db141bd276c35a4cd84d85c7f8c9dee0
mateusrodriguesilva <mateusrodrigues.contato01@gmail.com>
2021-01-29 19:11:13 -0300

    páginas dinâmicas, animações e Lottie - aula 05

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,9 +1,55 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import QuizScreen from '../../src/screens/Quiz'
 
-export default function QuizDaGaleraPage() {
+export default function QuizDaGaleraPage({ dbExterno }) {
+   /*  console.log(dbExterno) */
+
     return (
-        <div>
-            Desafio da próxima aula junto com as animações
-        </div>
+        /* <div> */
+            /* Desafio da próxima aula junto com as animações */
+            <ThemeProvider theme={dbExterno.theme}>
+            <QuizScreen 
+                externalQuestions={dbExterno.questions} 
+                externalBg={dbExterno.bg}
+            />
+            </ThemeProvider>
+            /* <pre style={ { color: 'black'}}>
+                {JSON.stringify(dbExterno.questions, null, 4)}
+            </pre> */
+        /* </div> */
     )
+}
+
+export async function getServerSideProps(context) {
+    const [projectName, githubUser] = context.query.id.split('___')
+    //console.log('Infos que o Next da para nós', context.query.id)
+
+    try  {
+        const dbExterno = await fetch (`https://${projectName}.${githubUser}.vercel.app/api/db`)
+             .then((respostaDoServer) => {
+                 if (respostaDoServer.ok) {
+                     return respostaDoServer.json()
+                 }
+                 throw new Error('Falhar em pegar os dados')
+             })
+             .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+             /* .catch((err) => {
+                 //console.log(err)
+             }) */
+     
+             //console.log('dbExterno',dbExterno)
+             //console.log('Infos que o Next da para nós', context.query.id)
+     
+         return {
+             props: {
+                 dbExterno,
+             },
+         }
+
+    } catch (err) {
+        //redirect ...
+        throw new Error(err)
+    }
+
 }
--- src/screens/Quiz/index.js
+++ src/screens/Quiz/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import QuizContainer from '../src/components/QuizContainer'
-import AlternativesForm from '../src/components/AlternativesForm'
-import Button from '../src/components/Button'
+import React from 'react';
+//import { Lottie } from '@crello/react-lottie';
+import db from '../../../db.json';
+import Widget from '../../components/Widget';
+import QuizLogo from '../../components/QuizLogo';
+import QuizBackground from '../../components/QuizBackground';
+import QuizContainer from '../../components/QuizContainer';
+import AlternativesForm from '../../components/AlternativesForm';
+import Button from '../../components/Button';
+import BackLinkArrow from '../../components/BackLinkArrow';
 
 function ResultWidget({ results }) {
   return (
@@ -73,7 +75,7 @@ function QuestionWidget({
         <Widget>
             
             <Widget.Header>
-              
+              <BackLinkArrow href="/" />
               <h3>
                 {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}    
               </h3>
@@ -163,14 +165,15 @@ function QuestionWidget({
     RESULT: 'RESULT',
   };
 
-export default function QuizPage() {
+export default function QuizPage({ externalQuestions, externalBg }) {
     
     const [screenState, setScreenState] = React.useState(screenStates.LOADING)
     const [results, setResults] = React.useState([])
-    const totalQuestions = db.questions.length
     const [currentQuestion, setCurrentQuestion] = React.useState(0)
     const questionIndex = currentQuestion
-    const question = db.questions[questionIndex]
+    const question = externalQuestions[questionIndex]
+    const totalQuestions = externalQuestions.length
+    const bg = externalBg
 
     function addResult(result) {
       setResults([
@@ -196,7 +199,7 @@ export default function QuizPage() {
     }
     
     return (
-        <QuizBackground backgroundImage={db.bg}>
+        <QuizBackground backgroundImage={bg}>
             <QuizContainer>
                 <QuizLogo />
                 {screenState === screenStates.QUIZ && (
